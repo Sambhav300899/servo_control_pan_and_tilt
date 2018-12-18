e630d63157b96c3705273038a3337ab9ae638d15
Sambhav300899 <sambhav300899@gmail.com>
2018-12-18 20:40:10 +0530

    "final code"

--- servo_control_pan_and_tilt.ino
+++ servo_control_pan_and_tilt.ino
@@ -1,31 +1,31 @@
 #include<ros.h>
-#include<std_msgs/Int64.h>
-#include<std_msgs/Int64.h>
+#include<std_msgs/Int16.h>
 #include<Servo.h>
 
 Servo ser1,ser2,ser3;
 int clk_anti_clk = 0;
-
-std_msgs::Int64 pan;
-std_msgs::Int64 tilt;
-int pos1 = 0,pos2 = 0,pos3 = 180;
+//servo will move for input 1
+std_msgs::Int16 pan;
+std_msgs::Int16 tilt;
+int pos1 = 0,pos2 = 0,pos3 = 170;
 ros::NodeHandle nh;
-int v = 10;
+int v = 15; //angle of rotation per input
 int ser12 = 0;
 ros::Publisher degree1("pan",&pan);
 ros::Publisher degree2("tilt",&tilt);
+int fin = 0;
 
 //bottom servo is servo1, middle is servo2, top is servo3
 //ser12 == 1 means move bottom servo if ser12 == 2 move middle servo
 
-void message( const std_msgs::Int64 &ms ){
+void message( const std_msgs::Int16 &ms ){
 
   int moveit = ms.data;
   if (moveit == 0)
-  { 
+  {
   //add code if needed
   }
-  
+
   else if(moveit == 1 && pos1 + pos2 < 180)
   {
   if (clk_anti_clk == 0)
@@ -52,12 +52,13 @@ void message( const std_msgs::Int64 &ms ){
   ser2.write(pos2);
   }
 
-
   if (pos1 + pos2 == 360)
   {
   clk_anti_clk = 1;
   pos3 = pos3 - 15;
   ser3.write(pos3);
+  pos1 = 180;
+  pos2 = 180;
   }
 
   else if (pos1 + pos2 == 0)
@@ -65,18 +66,42 @@ void message( const std_msgs::Int64 &ms ){
   clk_anti_clk = 0;
   pos3 = pos3 - 15;
   ser3.write(pos3);
+  pos1 = 0;
+  pos2 = 0;
   }
 
   pan.data = pos1 + pos2;
-  tilt.data = pos3;
+  tilt.data = 170 - pos3;
+
+  /*if(pos1 + pos2 == 180)
+  {
+  delay(30);  //double point
+  }*/
+
+  if(pos3 <= 180 - 45)
+  {
+  pos3 = 170;
+  ser3.write(pos3);
+  fin = 1;
+  }
+
+  if(fin == 1)
+  {
+  tilt.data = 2;
+  degree2.publish(&tilt);
+  int pos1 = 0,pos2 = 0,pos3 = 170;
+  ser1.write(pos1);
+  ser2.write(pos2);
+  ser3.write(pos3);
+  }
 
   degree1.publish(&pan);
   degree2.publish(&tilt);
-
-  delay(5); //add delay if needed with higher frequency data
+  //delay(3); //add delay if needed with higher frequency data
 }
-  
-ros::Subscriber<std_msgs::Int64> sub("servo_work", &message );
+
+//manual mode when value published on topic is 1 and auto if 0
+ros::Subscriber<std_msgs::Int16> sub("servo_work", &message);
 
 void setup() {
   // put your setup code here, to run once:
@@ -86,16 +111,13 @@ nh.advertise(degree1);
 nh.advertise(degree2);
 nh.subscribe(sub);
 ser1.attach(10);
-ser2.attach(9);
-ser3.attach(8);
+ser2.attach(6);
+ser3.attach(9);
 ser1.write(pos1);
 ser2.write(pos2);
 ser3.write(pos3);
 }
 
 void loop() {
-  
-  nh.spinOnce(); 
-    
-  // put your main code here, to run repeatedly:
+  nh.spinOnce();
 }
